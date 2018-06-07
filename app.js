7cf995dbcc65bccf8406a8b81acf4145ebc71e18
Mace <maceplus@gmail.com>
2018-06-07 19:07:50 +0100

    render added

--- app.js
+++ app.js
@@ -7,12 +7,12 @@ app.get('/', function(req, res) {
     res.render('index');
 });
 
-app.get('/timeTable', function(req, res) {
-    res.send('Time table coming soon!');
+app.get('/contacts', function(req, res) {
+    res.render('contacts');
 });
 
-app.get('/contacts', function(req, res) {
-    res.send('Contacts page coming soon!');
+app.get('/timeTable', function(req, res) {
+    res.send('Time table coming soon!');
 });
 
 
