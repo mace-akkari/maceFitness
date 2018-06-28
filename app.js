ae6020ed1c82c13ab4cde8aa81e8ead9ac7487cf
Patrick Orrell <orrell@gmail.com>
2018-06-28 19:11:26 +0100

    Session 3

--- app.js
+++ app.js
@@ -13,10 +13,23 @@ app.get('/contacts', function(req, res) {
 
 app.get('/timetable', function(req, res) {
     const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    const appointments = [
+        {
+            name: 'TEST',
+            time: 13,
+            day: 2
+        },
+        {
+            name: 'TEST 2',
+            time: 10,
+            day: 4
+        }
+    ];
     const timetableOptions = {
         days,
         openingTime: 6,
-        closingTime: 21
+        closingTime: 21,
+        appointments
     };
     res.render('timetable', timetableOptions);
 });
