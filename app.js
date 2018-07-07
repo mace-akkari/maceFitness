fbda8623f6e6b8247f8b85a3dc7aa81754de49aa
Mace <maceplus@gmail.com>
2018-07-08 00:19:19 +0100

    add Mongodb

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+const mongodb = require('mongodb').MongoClient;
+
+const DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+let db;
+mongodb.connect('mongodb://localhost:27017', (err, client) => {
+    if(err) {
+        console.error(err);
+    } 
+    db = client.db('mace');
+})
 
 app.set('view engine', 'pug');
 
@@ -12,13 +23,15 @@ app.get('/contacts', function(req, res) {
 });
 
 app.get('/timetable', function(req, res) {
-    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    const timetableOptions = {
-        days,
-        openingTime: 6,
-        closingTime: 21
-    };
-    res.render('timeTable', timetableOptions);
+    const days = DAYS;
+    db.collection('appointments').find().toArray((err, result) => {
+        const timetableOptions = {
+            days,
+            openingTime: 6,
+            closingTime: 21
+        };
+        res.render('timeTable', timetableOptions);
+    });
 });
 
 app.use('/stylesheets', express.static('views/stylesheets'));
